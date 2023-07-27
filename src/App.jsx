ad9ccfcdd7234c0fb3d6f45949cac80be832b6ec
Sukkoth <suukootj@gmail.com>
2023-07-27 11:06:35 +0300

    Star rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
 
 const tempMovieData = [
     {
@@ -149,28 +149,6 @@ function Movie({ movie }) {
         </li>
     );
 }
-
-// function WatchedBox() {
-//     const [watched, setWatched] = useState(tempWatchedData);
-//     const [isOpen2, setIsOpen2] = useState(true);
-
-//     return (
-//         <div className='box'>
-//             <button
-//                 className='btn-toggle'
-//                 onClick={() => setIsOpen2((open) => !open)}
-//             >
-//                 {isOpen2 ? '–' : '+'}
-//             </button>
-//             {isOpen2 && (
-//                 <>
-//                     <WatchedSummary watched={watched} />
-//                     <WatchedMoviesList watched={watched} />
-//                 </>
-//             )}
-//         </div>
-//     );
-// }
 
 function WatchedSummary({ watched }) {
     const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
---
+++ src/StarRating.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function StarRating({
+    maxRating = 5,
+    color = '#fcc419',
+    size = 40,
+    defaultRating = 0,
+    onSetRating,
+}) {
+    const [rating, setRating] = useState(defaultRating);
+    const [tempRating, setTempRating] = useState(defaultRating);
+
+    function handleRating(i) {
+        setRating(i + 1);
+        onSetRating(i + 1);
+    }
+
+    const textStyle = {
+        lineHeight: '1',
+        margin: '0',
+        color: color,
+        fontSize: `${size / 1.8}px`,
+    };
+    return (
+        <div style={containerStyle}>
+            <div style={starContainerStyle}>
+                {Array.from({ length: maxRating }, (_, i) => (
+                    <Star
+                        key={i}
+                        onRate={() => handleRating(i)}
+                        full={tempRating >= i + 1}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setTempRating(rating)}
+                        tempRating={tempRating}
+                        color={color}
+                        size={size}
+                    />
+                ))}
+            </div>
+            <p style={textStyle}>{rating || ''}</p>
+        </div>
+    );
+}
+
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+    const starStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+        display: 'block',
+        cursor: 'pointer',
+        color: 'yellow',
+    };
+    return (
+        <span
+            role='button'
+            style={starStyle}
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
+            <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                stroke={color}
+                fill={full ? color : '#f5f4f2'}
+            >
+                <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+            </svg>
+        </span>
+    );
+}
+
+const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '16px',
+    marginTop: '5px',
+};
+
+const starContainerStyle = {
+    display: 'flex',
+};
+
+StarRating.propTypes = {
+    maxRating: PropTypes.number.isRequired,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    defaultRating: PropTypes.number,
+    onSetRating: PropTypes.func.isRequired,
+};
